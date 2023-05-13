d7ffc0ca30589cca93d90d6f1d302aa9c051db21
thaddeusmbrown <33402458+thaddeusmbrown@users.noreply.github.com>
2023-05-13 15:37:47 -0500

    Get Exercise 2-8 rightrot working until 1-bits overflow.

--- Chapter 2: Types, Operators, and Expressions/2.9 Bitwise Operators/rightrot.c
+++ Chapter 2: Types, Operators, and Expressions/2.9 Bitwise Operators/rightrot.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+#include <limits.h>
 
 unsigned rightrot(unsigned x, int n);
 
 int main(int argc, char *argv[]) {
   int x = 216;
-  int n = 0;
+  int n = 4;
 
+  printf("The number of bits in an int is INT_BIT: %ld\n", sizeof(int)*CHAR_BIT);
   printf("x = %d\nn = %d\nAnswer is: %d\n", x, n, rightrot(x, n));
 }
 
-/* invert:  return x with the n bits that begin at position p (i.e., 1 changed into 
-   0 and vice versa), leaving the others unchanged. */
-unsigned invert(unsigned x, int p, int n) {
-  return x ^ (~(~0 << n) << (p + 1 - n));
+/* rightrot:  return x rotated to the right by n bit positions */
+unsigned rightrot(unsigned x, int n) {
+  unsigned left_out, right_out;
+  const unsigned long INT_BIT = sizeof(int)*CHAR_BIT;
+  right_out = x >> n;
+  left_out = x & (~0 << (INT_BIT - n));
+
+  printf("left_out:  %d\nright_out:  %d", left_out, right_out);
+
+  return left_out | right_out;
 }
